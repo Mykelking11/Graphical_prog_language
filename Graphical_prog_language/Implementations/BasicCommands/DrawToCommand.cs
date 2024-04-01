26b1cc919b55c9462be113077e5d9dfe7bee175a
mykelking11 <xlimmyke@gmail.com>
2024-04-01 18:43:16 +0100

    Draw to command

---
+++ Graphical_prog_language/Implementations/BasicCommands/DrawToCommand.cs
@@ -0,0 +1,33 @@
+﻿using Graphical_prog_language.Interface;
+
+namespace Graphical_prog_language.Implementations.BasicCommands
+{
+    /// <summary>
+    /// Represents a command to draw a line to a specified point on the canvas.
+    /// </summary>
+    public class DrawToCommand : IBasicCommand
+    {
+        /// <summary>
+        /// Executes the DrawTo command.
+        /// </summary>
+        /// <param name="canvas">The Canvas object representing the drawing surface.</param>
+        /// <param name="arguments">An array of arguments containing the coordinates of the destination point.</param>
+        public void Execute(Canvas canvas, string[] arguments)
+        {
+            // Implementation for DrawTo command
+            if (arguments.Length == 2 && int.TryParse(arguments[0], out int x) && int.TryParse(arguments[1], out int y))
+            {
+                Point destination = new Point(x, y);
+                using (Graphics graphics = canvas.DrawingPictureBox.CreateGraphics())
+                {
+                    graphics.DrawLine(canvas.DrawingPen, canvas.CurrentPosition, destination);
+                }
+                canvas.CurrentPosition = destination;
+            }
+            else
+            {
+                MessageBox.Show("Invalid arguments for 'drawto' command. Please provide a valid coordinates.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}

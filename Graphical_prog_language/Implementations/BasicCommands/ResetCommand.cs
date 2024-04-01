0e4ec7a1ac78e3f4e2e197392550c500a738261f
mykelking11 <xlimmyke@gmail.com>
2024-04-01 19:08:17 +0100

    Reset Command

---
+++ Graphical_prog_language/Implementations/BasicCommands/ResetCommand.cs
@@ -0,0 +1,27 @@
+﻿using Graphical_prog_language.Interface;
+
+namespace Graphical_prog_language.Implementations.BasicCommands
+{
+    /// <summary>
+    /// Represents a command to reset canvas settings.
+    /// </summary>
+    public class ResetCommand : IBasicCommand
+    {
+        public void Execute(Canvas canvas, string[] argument)
+        {
+            // Get command text box from the canvas
+            TextBox commandTextBox = canvas.CommandTextBox;
+
+            // Reset canvas properties
+            canvas.CurrentPosition = new Point(200, 150);
+            canvas.DrawingPen = new Pen(Color.Black);
+            canvas.FillColor = Color.Black;
+
+            // Use Invoke to update the commandTextBox on the UI thread
+            /*commandTextBox.Invoke((MethodInvoker)delegate
+            {
+                commandTextBox.Clear();
+            });*/
+        }
+    }
+}

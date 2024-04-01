3ac819351a6aa7a49cf80fa7acec480ab348b1f3
mykelking11 <xlimmyke@gmail.com>
2024-04-01 18:59:03 +0100

    Move To command

---
+++ Graphical_prog_language/Implementations/BasicCommands/MoveToCommand.cs
@@ -0,0 +1,28 @@
+﻿using Graphical_prog_language.Interface;
+
+namespace Graphical_prog_language.Implementations.BasicCommands
+{
+    /// <summary>
+    /// Represents a command to move the drawing position to a specified point on the canvas.
+    /// </summary>
+    public class MoveToCommand : IBasicCommand
+    {
+        /// <summary>
+        /// Executes the MoveTo command.
+        /// </summary>
+        /// <param name="canvas">The Canvas object representing the drawing surface.</param>
+        /// <param name="arguments">An array of arguments containing the coordinates of the destination point.</param>
+        public void Execute(Canvas canvas, string[] arguments)
+        {
+            // Implementation for MoveTo command
+            if (arguments.Length == 2 && int.TryParse(arguments[0], out int x) && int.TryParse(arguments[1], out int y))
+            {
+                canvas.CurrentPosition = new Point(x, y);
+            }
+            else
+            {
+                MessageBox.Show("Invalid arguments for 'moveto' command. Please provide valid coordinates.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}

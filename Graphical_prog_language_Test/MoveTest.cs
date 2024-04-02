216590c552ce534d90cf5e8614d3f99f30e31c94
mykelking11 <xlimmyke@gmail.com>
2024-04-02 19:02:46 +0100

    moveTo unit Test

---
+++ Graphical_prog_language_Test/MoveTest.cs
@@ -0,0 +1,39 @@
+﻿using Graphical_prog_language;
+using Graphical_prog_language.Implementations.BasicCommands;
+using System.Drawing;
+
+namespace Graphical_prog_language_Tests
+{
+    [TestFixture]
+    public class MoveTest
+    {
+        [Test]
+
+        public void Execute_WithValidArguments_SetsCurrentPosition()
+        {
+            // Arrange
+            var canvas = new Canvas(); // Assuming Canvas is a class provided by your application
+            var moveToCommand = new MoveToCommand();
+            string[] args = new string[] { "100", "200" }; // Valid arguments for coordinates
+
+            // Act
+            moveToCommand.Execute(canvas, args);
+
+            // Assert
+            Assert.AreEqual(new Point(100, 200), canvas.CurrentPosition);
+        }
+
+        [Test]
+        public void Execute_WithInvalidArguments_ShowErrorMessage()
+        {
+            // Arrange
+            var canvas = new Canvas();
+            var moveToCommand = new MoveToCommand();
+            string[] args = { "invalid", "50" };
+
+            // Act
+            moveToCommand.Execute(canvas, args);
+        }
+
+    }
+}

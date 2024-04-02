81868cb9d37ad1e5cc6cb31dbd19e49a3fd49c33
mykelking11 <xlimmyke@gmail.com>
2024-04-02 18:54:21 +0100

    Reset unit test

---
+++ Graphical_prog_language_Test/ResetTest.cs
@@ -0,0 +1,28 @@
+﻿using Graphical_prog_language.Implementations.BasicCommands;
+using Graphical_prog_language;
+using System.Drawing;
+
+namespace Graphical_prog_language_Tests
+{
+    [TestFixture]
+    public class ResetTest
+    {
+        [Test]
+
+        public void Execute_ResetsCanvasProperties()
+        {
+            // Arrange
+            ResetCommand resetCommand = new ResetCommand();
+            Canvas canvas = new Canvas();
+
+            // Act
+            resetCommand.Execute(canvas, new string[] { });
+
+            // Assert
+            Assert.AreEqual(new Point(200, 150), canvas.CurrentPosition);
+            Assert.AreEqual(Color.Black, canvas.DrawingPen.Color);
+            Assert.AreEqual(Color.Black, canvas.FillColor);
+
+        }
+    }
+}

3f040c995505071eabdcff31aec4d792f1108cb0
mykelking11 <xlimmyke@gmail.com>
2024-04-02 18:46:40 +0100

    drawto test

---
+++ Graphical_prog_language_Test/DrawTest.cs
@@ -0,0 +1,36 @@
+﻿using Graphical_prog_language.Implementations.BasicCommands;
+using Graphical_prog_language;
+using System.Drawing;
+
+namespace Graphical_prog_language_Tests
+{
+    [TestFixture]
+    public class DrawTest
+    {
+        [Test]
+        public void Execute_DrawsLineToSpecifiedPoint()
+        {
+            // Arrange
+            var canvas = new Canvas(); // Assuming Canvas is a class provided by your application
+            var drawToCommand = new DrawToCommand();
+            string[] arguments = new string[] { "100", "100" }; // Destination coordinates
+
+
+            // Act
+            drawToCommand.Execute(canvas, arguments);
+
+            // Assert
+            Assert.AreEqual(new Point(100, 100), canvas.CurrentPosition);
+        }
+
+        [Test]
+        public void Execute_WithInvalidArguments_ThrowsExceptionWithErrorMessage()
+        {
+            var canvas = new Canvas();
+            var drawToCommand = new DrawToCommand();
+            string[] args = { "invalid", "50" };
+
+            drawToCommand.Execute(canvas, args);
+        }
+    }
+}

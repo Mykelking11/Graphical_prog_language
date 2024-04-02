65eab6aec2165992faf12bb49d76763c9e59c6cd
mykelking11 <xlimmyke@gmail.com>
2024-04-02 18:59:58 +0100

    Fiil Unit Test

---
+++ Graphical_prog_language_Test/FillTest.cs
@@ -0,0 +1,60 @@
+﻿using Graphical_prog_language.Implementations.BasicCommands;
+using Graphical_prog_language;
+
+namespace Graphical_prog_language_Tests
+{
+    [TestFixture]
+    public class FillTest
+    {
+        [Test]
+
+        public void Execute_FillCommand_OnMode()
+        {
+            // Arrange
+            var canvas = new Canvas();
+            var fillCommand = new FillCommand();
+            string[] arguments = { "on" };
+
+            // Act
+            fillCommand.Execute(canvas, arguments);
+
+            // Assert
+            Assert.IsTrue(canvas.IsFilling);
+            Assert.AreEqual(canvas.DrawingPen.Color, canvas.FillColor);
+        }
+
+
+        [Test]
+        public void Execute_FillCommand_OffMode()
+        {
+            // Arrange
+            var canvas = new Canvas();
+            var fillCommand = new FillCommand();
+            string[] arguments = { "off" };
+
+            // Act
+            fillCommand.Execute(canvas, arguments);
+
+            // Assert
+            Assert.IsFalse(canvas.IsFilling);
+        }
+
+        [Test]
+        public void Execute_FillCommand_InvalidMode()
+        {
+            // Arrange
+            Canvas canvas = new Canvas();
+            FillCommand fillCommand = new FillCommand();
+            string[] arguments = { "invalid" }; // Using invalid mode
+
+            // Act
+            fillCommand.Execute(canvas, arguments);
+
+            // Assert
+            Assert.IsFalse(canvas.IsFilling);
+        }
+
+
+
+    }
+}

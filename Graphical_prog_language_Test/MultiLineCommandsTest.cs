a8d6022b0c86e16b90491b1e2286733fd1aec166
mykelking11 <xlimmyke@gmail.com>
2024-04-04 09:21:43 +0100

    Multiline Unit Test

---
+++ Graphical_prog_language_Test/MultiLineCommandsTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using Graphical_prog_language.Implementations;
+using Moq;
+using System.Drawing;
+using System.Windows.Forms;
+using Graphical_prog_language;
+
+namespace Graphical_prog_language_Tests
+{
+    [TestFixture]
+    public class MultiLineCommandsTests
+    {
+        private Canvas canvas;
+        private MultiLineCommands multiLineCommands;
+
+        [SetUp]
+        public void SetUp()
+        {
+            canvas = new Canvas();
+            multiLineCommands = new MultiLineCommands(canvas);
+        }
+
+        [Test]
+        public void ExecuteCommands_ValidScriptContent_DrawsShapes()
+        {
+            // Arrange
+            string scriptContent = "circle 10\nrectangle 20 30";
+
+            // Mock the canvas ShapeMaker
+            var mockShapeMaker = new Mock<ShapeMaker>();
+            canvas.ShapeMaker = mockShapeMaker.Object;
+
+            // Act
+            multiLineCommands.ExecuteCommands(scriptContent);
+
+            // Assert
+            mockShapeMaker.Verify(sm => sm.ExecuteDrawing(It.IsAny<CommandParser>()), Times.Exactly(2));
+        }
+
+        [Test]
+        public void ExecuteCommands_InvalidScriptContent_NoShapesDrawn()
+        {
+            // Arrange
+            string invalidScriptContent = "invalidCommand";
+
+            // Mock the canvas ShapeMaker
+            var mockShapeMaker = new Mock<ShapeMaker>();
+            canvas.ShapeMaker = mockShapeMaker.Object;
+
+            // Act
+            multiLineCommands.ExecuteCommands(invalidScriptContent);
+
+            // Assert
+            mockShapeMaker.Verify(sm => sm.ExecuteDrawing(It.IsAny<CommandParser>()), Times.Never());
+        }
+
+    }
+}
